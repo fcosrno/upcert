16dc89d19876865e9956a64f8ee5f7f1e2cb641e
Francisco Serrano <fserrano@gmail.com>
2018-04-07 20:49:03 -0400

    Get expire dates

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,33 @@
 import { spawn } from 'child_process';
-const command = spawn('docker', ['ps']);
+import { zip } from 'rxjs/observable/zip';
+import { Observable } from 'rxjs/Observable';
 
-console.log('Hi there now!');
+const observables: Array<Observable<any>> = [];
 
-command.stdout.on('data', function(data) {
-  console.log(data.toString());
+['google.com', 'amazon.com'].forEach(n => {
+  const command = spawn('sh', [
+    '-c',
+    `echo | openssl s_client -connect ${n}:443 -servername ${n} 2>/dev/null | openssl x509 -noout -enddate`
+  ]);
+  observables.push(
+    Observable.create(function(observer) {
+      command.stdout.on('data', function(data) {
+        const expirationDate = data
+          .toString()
+          .replace('notAfter=', '')
+          .replace('\n', '');
+        observer.next(`${n}: ${expirationDate}`);
+      });
+      command.stderr.on('data', function(data) {
+        observer.next(`${n}: Error`);
+      });
+    })
+  );
 });
-command.stderr.on('data', function(data) {
-  console.log(data.toString());
+
+zip(...observables).subscribe(val => {
+  console.log(val);
 });
+
+// TODO MVP: Email a report of expiration dates
+// TODO If expires date is within the week, email
