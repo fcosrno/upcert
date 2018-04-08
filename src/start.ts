d285704a9e0c79851df7dd4639f1d7aa27e2d9dd
Francisco Serrano <fserrano@gmail.com>
2018-04-07 22:15:01 -0400

    Use moment for dates

--- src/start.ts
+++ src/start.ts
@@ -2,14 +2,16 @@ import { spawn } from 'child_process';
 import { zip } from 'rxjs/observable/zip';
 import { Observable } from 'rxjs/Observable';
 import dotenv from 'dotenv';
+import * as moment from 'moment';
 
 const observables: Array<Observable<any>> = [];
 
-process.env.SITES.split(',').forEach(n => {
+process.env.SITES.split(',').forEach(site => {
   const command = spawn('sh', [
     '-c',
-    `echo | openssl s_client -connect ${n}:443 -servername ${n} 2>/dev/null | openssl x509 -noout -enddate`
+    `echo | openssl s_client -connect ${site}:443 -servername ${site} 2>/dev/null | openssl x509 -noout -enddate`
   ]);
+
   observables.push(
     Observable.create(function(observer) {
       command.stdout.on('data', function(data) {
@@ -17,17 +19,26 @@ process.env.SITES.split(',').forEach(n => {
           .toString()
           .replace('notAfter=', '')
           .replace('\n', '');
-        observer.next(`${n}: ${expirationDate}`);
+
+        const timeAgo = moment(new Date(expirationDate))
+          .endOf('day')
+          .fromNow();
+
+        observer.next({
+          site,
+          timeAgo,
+          message: `${site} expires ${timeAgo} on ${expirationDate}`
+        });
       });
       command.stderr.on('data', function(data) {
-        observer.next(`${n}: Error`);
+        observer.next(`${site}: Error`);
       });
     })
   );
 });
 
-zip(...observables).subscribe(val => {
-  console.log(val);
+zip(...observables).subscribe(report => {
+  console.log(report);
 });
 
 // TODO MVP: Email a report of expiration dates
